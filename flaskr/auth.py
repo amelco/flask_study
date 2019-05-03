32a50d67c2ad5d92775601e6747502a9a003291e
amelco <amelco.herman@gmail.com>
2019-05-03 18:26:58 -0300

    user registration, authentication and page styles

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -32,58 +32,61 @@ def register():
                 (username, generate_password_hash(password))
             )
             db.commit()
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('auth.register'))
 
         flash(error)
 
     return render_template('auth/register.html')
 
-    @bp.route('/login', methods=('GET', 'POST'))
-    def login():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            db = get_db()
-            error = None
-            user = db.execute(
-                    'SELECT * FROM user WHERE username = ?', (username,)
-            ).fetchone()
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+                'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
-            if user is None:
-                error = 'Usuário incorreto.'
-            elif not check_password_hash(user['password'], password):
-                error = 'Senha incorreta.'
+        if user is None:
+            error = 'Usuário não existe.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Senha incorreta.'
 
-            if error is None:
-                session.clear()
-                session['userid'] = user['id']
-                return redirect(url_for('index'))
+        if error is None:
+            session.clear()
+            session['userid'] = user['id']
+            return redirect(url_for('index'))
 
-            flash(error)
-        return render_template('auth/login.html')
+        flash(error)
+    return render_template('auth/login.html')
 
-    @bp.before_app_request
-    def load_logged_in_user():
-        user_id = session.get('user_id')
 
-        if user_id is None:
-            g.user = None
-        else:
-            g.user = get_db().execute(
-                    'SELECT * FROM user WHERE id = ?', (user_id,)
-            ).fetchone()
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
 
-    @bp.route('/logout')
-    def logout():
-        session.clear()
-        return redirect(url_for('index'))
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+                'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
-    def login_required(view):
-        @functools.wraps(view)
-        def wrapped_view(**kwargs):
-            if g.user is None:
-                return redirect(url_for('auth.login'))
 
-            return view(**kwargs)
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
-        return wrapped_view
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
